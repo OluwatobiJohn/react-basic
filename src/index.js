a0354bc19ee95bb57bb1ae1682104fe178b63441
OluwatobiJohn <46735761+OluwatobiJohn@users.noreply.github.com>
2021-03-04 17:52:51 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import BookList from "./BookList";
+
+//CSS
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BookList />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
